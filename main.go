b20433407c845f222f315f3eff3e63e50582f4e9
maker2413 <maker2413@gmail.com>
2025-05-18 00:22:38 -0500

    added config read in functionality

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("/etc/image-puller/")
+	viper.AddConfigPath("$HOME/.config/image-puller/")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("fatal error config file: %w", err))
+	}
+
+	if !viper.IsSet("images") {
+		panic(fmt.Errorf("fatal no images provided in config file!"))
+	}
+
+	images := viper.GetStringSlice("images")
+
+	fmt.Println(images)
 }
